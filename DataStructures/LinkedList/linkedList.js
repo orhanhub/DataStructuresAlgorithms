3b278adbc1143877cd2e25fe90da19d8dc790f52
Orhan Yarar <orhan@hotmail.ca>
2019-10-27 13:01:29 -0400

    Linked List Remove method implemented; 100% test coverage

--- DataStructures/LinkedList/linkedList.js
+++ DataStructures/LinkedList/linkedList.js
@@ -42,6 +42,54 @@ class LinkedList {
     }
     return this;
   }
+  /**
+   * Remove a node from the Linked List
+   * @param {*} input
+   * @return {LinkedList}
+   */
+  remove(input, node) {
+    //if node is not defined, start from the head;
+    node = node || this.head;
+
+    //Case: item to remove is the head
+    if (this.head.value === input) {
+      //Case: item to remove is also the tail,
+      // i.e. the linked list has single node;
+      if (this.tail.value === input) {
+        //then set the linked list is null
+        this.head = null;
+        this.tail = null;
+        return this;
+      }
+      // otherwise just move the head pointer to next node
+      this.head = this.head.next;
+      return this;
+    }
+
+    //Case: no item to remove
+    //if current item is the tail and no item found, just stop;
+    if (node.next === null) {
+      return this;
+    }
+
+    //Case: Move along the Link List to remove the item
+    //if current node's next item is the target to remove
+    //move the pointer to the subsequent item
+    if (node.next.value === input) {
+      //if the next item is the tail, first adjust the tail pointer
+      if (node.next.value === this.tail.value) {
+        this.tail = node;
+      }
+      //then move the pointer to the subsequent item
+      node.next = node.next.next;
+    }
+
+    //otherwise, call the remove function recursively
+    else {
+      return this.remove(input, node.next);
+    }
+    return this;
+  }
 }
 
 exports.LinkedList = LinkedList;
--- test/linkedList.spec.js
+++ test/linkedList.spec.js
@@ -33,4 +33,54 @@ describe("Linked List tests", function() {
     expect(linkedList.tail.next).to.equal(null);
     expect(linkedList.head.next).to.equal(linkedList.tail);
   });
+
+  it("can remove the head node", function() {
+    var n1 = new Node(1);
+    var n2 = new Node(2);
+    var n3 = new Node(3);
+    linkedList.insert(n1);
+    linkedList.insert(n2);
+    linkedList.insert(n3);
+    linkedList.remove(1);
+    expect(linkedList.head.value).to.equal(2);
+  });
+
+  it("can remove a node", function() {
+    var n1 = new Node(1);
+    var n2 = new Node(2);
+    var n3 = new Node(3);
+    linkedList.insert(n1);
+    linkedList.insert(n2);
+    linkedList.insert(n3);
+    linkedList.remove(2);
+    expect(linkedList.head.value).to.equal(1);
+    expect(linkedList.tail.value).to.equal(3);
+  });
+
+  it("can remove the tail node", function() {
+    var n1 = new Node(1);
+    var n2 = new Node(2);
+    var n3 = new Node(3);
+    linkedList.insert(n1);
+    linkedList.insert(n2);
+    linkedList.insert(n3);
+    linkedList.remove(3);
+    expect(linkedList.tail.value).to.equal(2);
+  });
+
+  it("can remove the single node linked list", function() {
+    var n1 = new Node(1);
+    linkedList.insert(n1);
+    linkedList.remove(1);
+    expect(linkedList.head).to.equal(null);
+    expect(linkedList.tail).to.equal(null);
+  });
+
+  it("returns the linked list if no item found", function() {
+    var n1 = new Node(1);
+    linkedList.insert(n1);
+    linkedList.remove(2);
+    expect(linkedList.head.value).to.equal(1);
+    expect(linkedList.head).to.equal(linkedList.tail);
+  });
 });
